a93a7da2ff1f751d24ad6d25763c360f86c35204
ClassyCoder1 <classycoder1@gmail.com>
2024-04-16 17:33:45 +0530

    Added join message.

--- src/main/java/com/hypixelremakeproject/staffchat/events/PlayerJoin.java
+++ src/main/java/com/hypixelremakeproject/staffchat/events/PlayerJoin.java
@@ -3,8 +3,10 @@ package com.hypixelremakeproject.staffchat.events;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 
 import com.hypixelremakeproject.staffchat.StaffChat;
 
@@ -16,10 +18,13 @@ public class PlayerJoin implements Listener {
 		this.plugin = plugin;
 	}
 	
+	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		if (!(player.hasPermission("staffchat.sc"))) {
 			player.sendMessage(ChatColor.RED + "You need " + ChatColor.BLUE +"JR HELPER " +ChatColor.RED + "to do this.");
+		} else {
+			Bukkit.broadcast(ChatColor.AQUA + "[STAFF] " + player.getName() + ChatColor.YELLOW + "joined!", "staffchat.sc");
 		}
 	}
 }
